efa777477b0604fb61404469304b81012427a007
Mohammad Equaan Kacchi <equaankacchi367@gmail.com>
2026-03-18 15:19:48 +0530

    Delete iac directory

--- iac/terraform/aws/base/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "client_name" {
-  description = "The client name passed through"
-  value       = var.client_name
-}
-
-output "aws_region" {
-  description = "The AWS region being used"
-  value       = var.aws_region
-}
