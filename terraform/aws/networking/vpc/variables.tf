6961de326fad10bd6be6e8a163d6762ecf90460a
Mohammad Equaan Kacchi <equaankacchi367@gmail.com>
2026-03-18 15:21:10 +0530

    Create variables.tf

---
+++ terraform/aws/networking/vpc/variables.tf
@@ -0,0 +1,10 @@
+variable "client_name" {
+  description = "The name of the client"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
